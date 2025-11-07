326720ee5d085ec978218e7d72ebe88a595658b5
Pratik Rana <pratikrana62049@gmail.com>
2025-11-07 19:46:01 +0530

    Finally done

--- client/src/pages/Bookmark.jsx
+++ client/src/pages/Bookmark.jsx
@@ -147,7 +147,7 @@ return (
                     ))}
                 </div>
             ):<>
-                <div className="flex w-full min-h-screen justify-center items-center gap-2">
+                <div className="flex w-full justify-center items-center gap-2">
                     <div className="w-8 h-8 bg-blue-500 border-dashed rounded-full animate-spin"></div>
                     <div className="w-8 h-8 bg-red-500  border-dashed rounded-full animate-spin"></div>
                     <div className="w-8 h-8 bg-green-500  border-dashed rounded-full animate-spin"></div>
