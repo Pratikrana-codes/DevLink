d79ff9eb7e65672f9998e2cd5830f8c0af520c3c
Pratik Rana <pratikrana62049@gmail.com>
2025-11-07 19:39:53 +0530

    Loader added

--- client/src/pages/Bookmark.jsx
+++ client/src/pages/Bookmark.jsx
@@ -147,10 +147,10 @@ return (
                     ))}
                 </div>
             ):<>
-                <div className="flex w-full h-full justify-center items-center">
-                        <div className="w-8 h-8 bg-blue-500 border-dashed rounded-full animate-spin"></div>
-                        <div className="w-8 h-8 bg-red-500  border-dashed rounded-full animate-spin"></div>
-                        <div className="w-8 h-8 bg-green-500  border-dashed rounded-full animate-spin"></div>
+                <div className="flex justify-center items-center">
+                    <div className="w-12 h-12 bg-blue-500 border-dashed rounded-full animate-spin"></div>
+                    <div className="w-12 h-12 bg-red-500  border-dashed rounded-full animate-spin"></div>
+                    <div className="w-12 h-12 bg-green-500  border-dashed rounded-full animate-spin"></div>
                 </div>
             </>}
             </div>
